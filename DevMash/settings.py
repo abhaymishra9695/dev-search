0e66f125a10c6a31ac66ffc809f936bca95cc433
sharanbrar <sharanbrar@gmail.com>
2023-09-18 23:26:31 +0530

    change

--- DevMash/settings.py
+++ DevMash/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIR = {
-    os.path.join(BASE_DIR , "public/static")
+    os.path.join(BASE_DIR , "static")
 }
 
 
